fbae864cb8ffa152e3be23412636e76947699648
ghost1 <sonofagronom@gmail.com>
2022-01-10 21:47:54 +0700

    Added cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'cloudinary',
-    providerOptions: {
+    config:{
+      provider: 'cloudinary',
+      providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET')
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
     },
   },
-})
+});
