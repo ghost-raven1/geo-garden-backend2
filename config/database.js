92b7c4408a9a609837eff0ec33f26859e30781b4
ghost1 <sonofagronom@gmail.com>
2022-01-10 16:21:16 +0700

    Added postress DB

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'ec2-34-249-49-9.eu-west-1.compute.amazonaws.com',
-      port: '5432',
-      database: 'da59jdjuscmb2j',
-      user: 'xyyzfflpnaoekv',
-      password: '61ff233dbf4807a382f4a490ca0e1329eb251e84619836b1b42dd8189ec9bb4e',
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false
       },
