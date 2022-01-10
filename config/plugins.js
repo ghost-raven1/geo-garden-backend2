02a01ebe2f6526472a241cc758c5bf2a3dd40354
ghost1 <sonofagronom@gmail.com>
2022-01-10 21:51:00 +0700

    Added cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,20 +1,10 @@
 module.exports = ({ env }) => ({
   upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {
-          folder: "geo-garden",
-          use_filename: true,
-          unique_filename: false,
-        },
-        delete: {},
-      },
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_KEY'),
+      api_secret: env('CLOUDINARY_SECRET'),
     },
   },
 });
